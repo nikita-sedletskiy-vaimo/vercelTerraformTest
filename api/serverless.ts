d8b9178f3f87ae061156c7bc81ab1a46d519366d
nikitasedletskiy <nikita.sedletskiy@vaimo.com>
2023-02-15 16:09:59 +0200

    Fix

--- api/serverless.ts
+++ api/serverless.ts
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../src/app"));
+app.register(import("../src/index"));
 
 export default async (req, res) => {
     await app.ready();
